51599def67146b2d3c9b24e899c811bcc81a9ace
GerardTrabazos <gerardtrabazos@inspladelestany.cat>
2025-01-19 20:01:02 +0100

    primer commit

--- app/src/main/java/cat/dam/gerard/app/ui/screens/Layout2.kt
+++ app/src/main/java/cat/dam/gerard/app/ui/screens/Layout2.kt
@@ -1,25 +1,46 @@
 package cat.dam.gerard.app.ui.screens
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import cat.dam.gerard.app.ui.theme.AppTheme
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Preview(showBackground = true)
 @Composable
 fun Layout2() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Green),
-        contentAlignment = Alignment.Center
+    Row (modifier = Modifier
+        .fillMaxSize()
+        .fillMaxWidth(),
     ) {
-        Text("Layout 2", color = Color.White)
+        Text(text = "Gerard", modifier = Modifier
+            .background(Color.Red)
+            .fillMaxWidth()
+            .fillMaxSize()
+            .weight(1f)
+            .wrapContentSize(Alignment.Center),
+        )
+        Text(text = "Gerard", modifier = Modifier
+            .background(Color.Green)
+            .fillMaxWidth()
+            .fillMaxSize()
+            .weight(1f)
+            .wrapContentSize(Alignment.Center)
+        )
+        Text(text = "Gerard", modifier = Modifier
+            .background(Color.Blue)
+            .fillMaxWidth()
+            .fillMaxSize()
+            .weight(1f)
+            .wrapContentSize(Alignment.Center)
+        )
     }
+
 }
 
 @Composable
